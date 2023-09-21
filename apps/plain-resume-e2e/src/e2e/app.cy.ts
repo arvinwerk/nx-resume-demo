d433656cde504506a0904123812f92a3a269da55
Arvin Sokarijo <arvin@webavance.nl>
2023-09-21 20:51:00 +0200

    work in progress

--- apps/plain-resume-e2e/src/e2e/app.cy.ts
+++ apps/plain-resume-e2e/src/e2e/app.cy.ts
@@ -3,11 +3,24 @@ import { getGreeting } from '../support/app.po';
 describe('plain-resume', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('Should filter list', () => {
+    console.log(cy.get('nx-resume-projects').children());
+    cy.get('nx-resume-projects')
+  .find('> div > div')
+  .then(listing => {
+    console.log('listing', listing);
+    console.log('listing L', listing.length);
+    const currentCount = listing.length;
+    cy.get('button').first().click();
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome plain-resume');
+    cy.get('nx-resume-projects')
+  .find('> div > div')
+  .then(listing => {
+    expect(listing.length).to.lessThan(currentCount);
+    listing.each((list) => {
+      // expect(list).contains()
+    })
+  });
+  });
   });
 });
